920b3f2069ad5391b3f0f61fc2eefa4e3c14f1d1
elbek9528 <elbek952812@gmail.com>
2023-04-15 22:20:22 +0500

    feat: matrix-el and rec-depth solved

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,8 +16,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const columns = matrix[0].length;
+  const rows = matrix.length;
+
+  for (let j = 0; j < columns; j++) {
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][j] === 0) {
+        break;
+      }
+      sum += matrix[i][j];
+    }
+  }
+
+  return sum;
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
